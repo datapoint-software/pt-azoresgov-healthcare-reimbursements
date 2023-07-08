3992813cd6f79feec2242ef4a7f7617b061a1900
Joao Lopes <joao.pl.lopes@datapoint.pt>
2023-07-08 15:48:26 +0100

    Loading overlay component

---
+++ src/angular/src/app/components/loading-overlay/loading-overlay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-loading-overlay',
+  standalone: true,
+  styleUrls: [ './loading-overlay.component.scss' ],
+  templateUrl: './loading-overlay.component.html'
+})
+export class LoadingOverlayComponent {
+
+}
