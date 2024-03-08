56adb434551d6656efec28e1360cf9ca389059a8
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-03-08 00:40:24 +0000

    Product version visible on welcome screen

--- src/angular/src/app/components/home/home.component.ts
+++ src/angular/src/app/components/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from "@angular/core";
+import { EnvironmentFeature } from "../../features/environment/environment.feature";
+import { CommonModule } from "@angular/common";
 
 @Component({
   imports: [
+    CommonModule
   ],
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  constructor(
+    private readonly environment: EnvironmentFeature
+  ) {}
+
+  readonly productVersion$ = this.environment.productVersion$;
 }
