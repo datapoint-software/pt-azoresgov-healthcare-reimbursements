6e71b5435db9c68f73af77e2d67dc2b17e042284
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-03-21 10:28:49 +0000

    dotnet:middleware:feat: process capture simululation query validator

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware/Features/ProcessCapture/ProcessCaptureSimulationQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.ProcessCapture
+{
+    public sealed class ProcessCaptureSimulationQueryValidator : AbstractValidator<ProcessCaptureSimulationQuery>
+    {
+        public ProcessCaptureSimulationQueryValidator()
+        {
+            RuleFor(e => e.UserId)
+                .NotEmpty();
+
+            RuleFor(e => e.ProcessId)
+                .NotEmpty();
+
+            RuleFor(e => e.ProcessRowVersionId)
+                .NotEmpty();
+        }
+    }
+}
