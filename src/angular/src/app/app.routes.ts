27bd40880f802bd7fd136266a476b815376f8f99
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-03-31 23:04:02 +0100

    Sign in route

--- src/angular/src/app/app.routes.ts
+++ src/angular/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignInGuard } from './guards/sign-in/sign-in.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    canActivate: [ SignInGuard.canActivate ],
+    component: SignInComponent,
+    path: 'sign-in',
+    providers: SignInGuard.providers
+  }
+];
