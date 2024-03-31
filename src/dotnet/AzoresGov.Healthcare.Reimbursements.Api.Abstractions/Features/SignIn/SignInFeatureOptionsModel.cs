9b8f70754184e9b422d367abff7f402ce8c713f1
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-03-31 23:22:52 +0100

    Sign in options endpoint contracts

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Api.Abstractions/Features/SignIn/SignInFeatureOptionsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AzoresGov.Healthcare.Reimbursements.Api.Features.SignIn
+{
+    public sealed class SignInFeatureOptionsModel
+    {
+        public SignInFeatureOptionsModel(bool persistentSessionsEnabled)
+        {
+            PersistentSessionsEnabled = persistentSessionsEnabled;
+        }
+
+        public bool PersistentSessionsEnabled { get; }
+    }
+}
---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Api/Features/SignIn/SignInFeatureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzoresGov.Healthcare.Reimbursements.Api.Features.SignIn
+{
+    [Route("/api/features/sign-in")]
+    public sealed class SignInFeatureController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public Task<SignInFeatureOptionsModel> GetOptionsAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
