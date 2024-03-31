ff5a8c057dfca0dac26b127be26dfb4750c87d49
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-03-31 23:50:47 +0100

    Sign in options middleware implementation

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware.Abstractions/Features/SignIn/SignInFeatureOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.SignIn
+{
+    public class SignInFeatureOptions
+    {
+        public SignInFeatureOptions(bool persistentSessionsEnabled)
+        {
+            PersistentSessionsEnabled = persistentSessionsEnabled;
+        }
+
+        public bool PersistentSessionsEnabled { get; }
+    }
+}
---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware/Features/SignIn/SignInOptionsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AzoresGov.Healthcare.Reimbursements.Management;
+using Datapoint.Mediator;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.SignIn
+{
+    public sealed class SignInOptionsQueryHandler : IQueryHandler<SignInFeatureOptionsQuery, SignInFeatureOptions>
+    {
+        private readonly IParameterManager _parameters;
+
+        public SignInOptionsQueryHandler(IParameterManager parameters)
+        {
+            _parameters = parameters;
+        }
+
+        public async Task<SignInFeatureOptions> HandleQueryAsync(SignInFeatureOptionsQuery query, CancellationToken ct)
+        {
+            var persistentSessionsEnabled = await _parameters.GetPersistentSessionsEnabledAsync(ct);
+
+            return new SignInFeatureOptions(
+                persistentSessionsEnabled);
+        }
+    }
+}
