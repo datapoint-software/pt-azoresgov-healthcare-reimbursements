183143e0d53f23e98d2e0c80dd3f3f3425c08c15
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-03-31 23:37:20 +0100

    Parameter repository

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork.Abstractions/Repositories/IParameterRepository.cs
@@ -0,0 +1,12 @@
+﻿using AzoresGov.Healthcare.Reimbursements.UnitOfWork.Entities;
+using Datapoint.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
+{
+    public interface IParameterRepository : IRepository<Parameter>
+    {
+        Task<Parameter?> GetByNameAsync(string name, CancellationToken ct);
+    }
+}
---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork/Repositories/ParameterRepository.cs
@@ -0,0 +1,19 @@
+﻿using AzoresGov.Healthcare.Reimbursements.UnitOfWork.Entities;
+using Datapoint.UnitOfWork.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
+{
+    public sealed class ParameterRepository : EntityFrameworkCoreRepository<HealthcareReimbursementsUnitOfWork, Parameter>, IParameterRepository
+    {
+        public ParameterRepository(HealthcareReimbursementsUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public Task<Parameter?> GetByNameAsync(string name, CancellationToken ct) =>
+
+            Entities.FirstOrDefaultAsync(e => e.Name == name, ct);
+    }
+}
