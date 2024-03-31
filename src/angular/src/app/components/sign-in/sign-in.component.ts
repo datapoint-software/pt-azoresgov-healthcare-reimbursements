ff1313f69dce996b7121ce5b2bf4259146142b90
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-03-31 23:03:26 +0100

    Sign in component

---
+++ src/angular/src/app/components/sign-in/sign-in.component.ts
@@ -0,0 +1,53 @@
+import { Component } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { SignInError, SignInForm } from "../../features/sign-in/sign-in-feature.abstractions";
+import { SignInFeature } from "../../features/sign-in/sign-in.feature";
+
+@Component({
+  imports: [
+    ReactiveFormsModule
+  ],
+  selector: 'app-sign-in',
+  standalone: true,
+  templateUrl: './sign-in.component.html'
+})
+export class SignInComponent {
+
+  // #region State
+
+  private _passwordVisible: boolean = false;
+
+  // #endregion
+
+  // #region State accessors
+
+  public get error(): SignInError | null {
+    return this._signIn.error;
+  }
+
+  public get form(): SignInForm {
+    return this._signIn.form;
+  }
+
+  public get passwordVisible(): boolean {
+    return this._passwordVisible;
+  }
+
+  // #endregion
+
+  // #region Actions
+
+  public togglePasswordVisible(): void {
+    this._passwordVisible = !this._passwordVisible;
+  }
+
+  public async submit(): Promise<boolean> {
+    return this._signIn.submit();
+  }
+
+  // #endregion
+
+  constructor(
+    private readonly _signIn: SignInFeature
+  ) {}
+}
