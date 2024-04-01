c6af18cc2aa501572b044a9d3c7208133fd887b2
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 07:10:57 +0100

    Claims principal helper initial implementation

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Api/Helpers/ClaimsPrincipalHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace AzoresGov.Healthcare.Reimbursements.Api.Helpers
+{
+    internal static class ClaimsPrincipalHelper
+    {
+        internal static Guid GetId(this ClaimsPrincipal principal) =>
+
+            Guid.Parse(principal.Claims
+                .First(c => c.Type.Equals(
+                    ClaimTypes.NameIdentifier,
+                    StringComparison.OrdinalIgnoreCase))
+                .Value);
+
+        internal static Guid GetSessionId(this ClaimsPrincipal principal) =>
+
+            Guid.Parse(principal.Claims
+                .First(c => c.Type.Equals(
+                    ClaimTypes.Sid,
+                    StringComparison.OrdinalIgnoreCase))
+                .Value);
+    }
+}
