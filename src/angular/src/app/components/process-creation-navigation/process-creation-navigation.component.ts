d36b7a0bd182a9e7f752a786f9e8001d253fe890
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 18:43:07 +0100

    Process creation navigation component router links

--- src/angular/src/app/components/process-creation-navigation/process-creation-navigation.component.ts
+++ src/angular/src/app/components/process-creation-navigation/process-creation-navigation.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
+  imports: [ RouterLink ],
   selector: 'app-process-creation-navigation',
   standalone: true,
   templateUrl: './process-creation-navigation.component.html'
