ef20d1da92ff44c2116524a651e011a7bfedf399
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 18:37:58 +0100

    Process creation entity selection component bootstrap

---
+++ src/angular/src/app/components/process-creation-entity/process-creation-entity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-process-creation-entity',
+  standalone: true,
+  templateUrl: './process-creation-entity.component.html'
+})
+export class ProcessCreationEntityComponent {
+
+}
