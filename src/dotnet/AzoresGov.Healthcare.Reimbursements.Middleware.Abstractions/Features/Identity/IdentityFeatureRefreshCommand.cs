31f6de01b0d993be16dcf233a3b1f196db7c7a51
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 06:47:46 +0100

    Identity feature refresh middleware contracts

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware.Abstractions/Features/Identity/IdentityFeatureRefreshCommand.cs
@@ -0,0 +1,18 @@
+﻿using Datapoint.Mediator;
+using System;
+
+namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.Identity
+{
+    public sealed class IdentityFeatureRefreshCommand : Command<IdentityFeatureRefreshResult>
+    {
+        public IdentityFeatureRefreshCommand(Guid userId, Guid userSessionId)
+        {
+            UserId = userId;
+            UserSessionId = userSessionId;
+        }
+
+        public Guid UserId { get; }
+
+        public Guid UserSessionId { get; }
+    }
+}
---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware.Abstractions/Features/Identity/IdentityFeatureRefreshResult.cs
@@ -0,0 +1,15 @@
+﻿namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.Identity
+{
+    public sealed class IdentityFeatureRefreshResult
+    {
+        public IdentityFeatureRefreshResult(IdentityFeatureRefreshResultUser user, IdentityFeatureRefreshResultUserSession userSession)
+        {
+            User = user;
+            UserSession = userSession;
+        }
+
+        public IdentityFeatureRefreshResultUser User { get; }
+
+        public IdentityFeatureRefreshResultUserSession UserSession { get; }
+    }
+}
