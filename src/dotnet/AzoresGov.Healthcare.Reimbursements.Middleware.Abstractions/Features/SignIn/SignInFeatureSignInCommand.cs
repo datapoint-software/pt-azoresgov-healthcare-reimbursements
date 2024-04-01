59f56872951cbbcbef0a1a0aefd0ea22d92ad37c
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 04:12:29 +0100

    Sign in middleware contracts

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware.Abstractions/Features/SignIn/SignInFeatureSignInCommand.cs
@@ -0,0 +1,27 @@
+﻿using Datapoint.Mediator;
+using System.Net;
+
+namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.SignIn
+{
+    public sealed class SignInFeatureSignInCommand : Command<SignInFeatureSignInResult>
+    {
+        public SignInFeatureSignInCommand(string userAgent, IPAddress remoteAddress, string emailAddress, string password, bool persistent)
+        {
+            UserAgent = userAgent;
+            RemoteAddress = remoteAddress;
+            EmailAddress = emailAddress;
+            Password = password;
+            Persistent = persistent;
+        }
+
+        public string UserAgent { get; }
+
+        public IPAddress RemoteAddress { get; }
+
+        public string EmailAddress { get; }
+
+        public string Password { get; }
+
+        public bool Persistent { get; }
+    }
+}
---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware.Abstractions/Features/SignIn/SignInFeatureSignInResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.SignIn
+{
+    public sealed class SignInFeatureSignInResult
+    {
+        public SignInFeatureSignInResult(Guid id, Guid rowVersionId, string name, string emailAddress, DateTimeOffset? expiration)
+        {
+            Id = id;
+            RowVersionId = rowVersionId;
+            Name = name;
+            EmailAddress = emailAddress;
+            Expiration = expiration;
+        }
+
+        public Guid Id { get; }
+
+        public Guid RowVersionId { get; }
+
+        public string Name { get; }
+
+        public string EmailAddress { get; }
+
+        public DateTimeOffset? Expiration { get; }
+    }
+}
