bde5d353a53e8f4897aa3477ae6c70e05e222642
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 07:27:27 +0100

    Conflict api helper signature update

--- src/angular/src/app/api/api.helpers.ts
+++ src/angular/src/app/api/api.helpers.ts
@@ -1,14 +1,14 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { ErrorResponseModel } from "./api.abstractions";
 
-export const conflict = (
-  fn: (model: ErrorResponseModel, response: HttpErrorResponse) => Promise<void> | void
-): ((error: unknown) => Promise<undefined>) => async (error) => {
+export const conflict = <T>(
+  fn: (model: ErrorResponseModel, response: HttpErrorResponse) => Promise<T> | T
+): ((error: unknown) => Promise<T>) => async (error) => {
 
   if (error instanceof HttpErrorResponse && error.status === 409) {
     const r = fn(error.error, error);
     if (r) await r;
-    return undefined;
+    return r;
   }
 
   throw error;
