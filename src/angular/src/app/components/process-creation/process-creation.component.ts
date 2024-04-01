41b57479e126747d3509cd07b8fe34d579466837
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 18:23:02 +0100

    Process creation component bootstrap

---
+++ src/angular/src/app/components/process-creation/process-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  imports: [ RouterOutlet ],
+  selector: 'app-process-creation',
+  standalone: true,
+  templateUrl: './process-creation.component.html'
+})
+export class ProcessCreationComponent {
+
+}
