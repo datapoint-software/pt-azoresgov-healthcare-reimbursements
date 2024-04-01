5428aa8c8a70987254a1fc22355c1d68be777c65
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 07:39:16 +0100

    Home component routing

--- src/angular/src/app/app.routes.ts
+++ src/angular/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignInGuard } from './guards/sign-in/sign-in.guard';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   {
@@ -8,5 +10,17 @@ export const routes: Routes = [
     component: SignInComponent,
     path: 'sign-in',
     providers: SignInGuard.providers
+  },
+  {
+    component: LayoutComponent,
+    path: '',
+    pathMatch: 'prefix',
+    children: [
+      {
+        component: HomeComponent,
+        path: '',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
