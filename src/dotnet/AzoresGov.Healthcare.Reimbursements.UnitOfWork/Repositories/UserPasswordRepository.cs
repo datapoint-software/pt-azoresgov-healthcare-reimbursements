c52146a7409d15a207f2f618fe9d6571d6018927
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 04:18:40 +0100

    User password repository

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork/Repositories/UserPasswordRepository.cs
@@ -0,0 +1,12 @@
+﻿using AzoresGov.Healthcare.Reimbursements.UnitOfWork.Entities;
+using Datapoint.UnitOfWork.EntityFrameworkCore;
+
+namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
+{
+    public sealed class UserPasswordRepository : EntityFrameworkCoreRepository<HealthcareReimbursementsUnitOfWork, UserPassword>, IUserPasswordRepository
+    {
+        public UserPasswordRepository(HealthcareReimbursementsUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
