dea8ab35e9289dfdb486fd6134d3e9bc90b3dae7
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 18:40:44 +0100

    Process creation confirmation boostrap

---
+++ src/angular/src/app/components/process-creation-confirmation/process-creation-confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-process-creation-confirmation',
+  standalone: true,
+  templateUrl: './process-creation-confirmation.component.html'
+})
+export class ProcessCreationConfirmationComponent {
+
+}
