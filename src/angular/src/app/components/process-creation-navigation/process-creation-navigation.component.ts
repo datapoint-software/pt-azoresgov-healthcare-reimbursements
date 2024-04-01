d9ef77cf89fc7910f1a15c5d5a2f7a81ae0076ce
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 18:30:24 +0100

    Process creation navigation component bootstrap

---
+++ src/angular/src/app/components/process-creation-navigation/process-creation-navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-process-creation-navigation',
+  standalone: true,
+  templateUrl: './process-creation-navigation.component.html'
+})
+export class ProcessCreationNavigationComponent {
+
+}
