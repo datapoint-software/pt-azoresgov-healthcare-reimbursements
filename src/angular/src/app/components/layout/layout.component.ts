310d44f3c32d73b4ca554bb88fdf68618036db90
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 08:08:33 +0100

    Layout initial implementation

--- src/angular/src/app/components/layout/layout.component.ts
+++ src/angular/src/app/components/layout/layout.component.ts
@@ -1,12 +1,44 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from "@angular/router";
+import { Subscription, filter, map } from "rxjs";
 
 @Component({
-  imports: [ RouterOutlet ],
+  imports: [ RouterLink, RouterOutlet ],
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.component.html'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnDestroy, OnInit {
 
+  // #region State
+
+  private _subscription: Subscription = undefined!;
+
+  private _url: string = undefined!;
+
+  // #endregion
+
+  // #region State accessors
+
+  public get url(): string {
+    return this._url ?? '/';
+  }
+
+  // #endregion
+
+  constructor(
+    private readonly _router: Router
+  ) {}
+
+  public ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+    this._subscription = undefined!;
+  }
+
+  public ngOnInit(): void {
+    this._subscription = this._router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .pipe(map(e => (e as NavigationEnd).url))
+      .subscribe((url) => this._url = url);
+  }
 }
