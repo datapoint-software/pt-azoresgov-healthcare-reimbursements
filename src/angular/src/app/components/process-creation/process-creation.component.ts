d24e0f48070fda2e0b827ab2e16e17c2dea25740
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 18:30:39 +0100

    Process creation navigation element inclusion

--- src/angular/src/app/components/process-creation/process-creation.component.ts
+++ src/angular/src/app/components/process-creation/process-creation.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { ProcessCreationNavigationComponent } from "../process-creation-navigation/process-creation-navigation.component";
 
 @Component({
-  imports: [ RouterOutlet ],
+  imports: [ ProcessCreationNavigationComponent, RouterOutlet ],
   selector: 'app-process-creation',
   standalone: true,
   templateUrl: './process-creation.component.html'
