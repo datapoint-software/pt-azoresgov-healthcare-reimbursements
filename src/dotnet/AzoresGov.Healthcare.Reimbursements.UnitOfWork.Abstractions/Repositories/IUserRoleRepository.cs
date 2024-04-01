3e2886dea3735fd937773b7a27ec8760a66413d2
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 19:06:39 +0100

    User roles by user identifier repository implementation

--- src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork.Abstractions/Repositories/IUserRoleRepository.cs
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork.Abstractions/Repositories/IUserRoleRepository.cs
@@ -1,9 +1,13 @@
 ﻿using AzoresGov.Healthcare.Reimbursements.UnitOfWork.Entities;
 using Datapoint.UnitOfWork;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
 {
     public interface IUserRoleRepository : IRepository<UserRole>
     {
+        Task<IReadOnlyCollection<UserRole>> GetAllByUserIdAsync(long userId, CancellationToken ct);
     }
 }
--- src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork/Repositories/UserRoleRepository.cs
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork/Repositories/UserRoleRepository.cs
@@ -1,5 +1,10 @@
 ﻿using AzoresGov.Healthcare.Reimbursements.UnitOfWork.Entities;
 using Datapoint.UnitOfWork.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
 {
@@ -8,5 +13,9 @@ namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
         public UserRoleRepository(HealthcareReimbursementsUnitOfWork unitOfWork) : base(unitOfWork)
         {
         }
+
+        public async Task<IReadOnlyCollection<UserRole>> GetAllByUserIdAsync(long userId, CancellationToken ct) =>
+
+            await Entities.Where(e => e.UserId == userId).ToListAsync(ct);
     }
 }
