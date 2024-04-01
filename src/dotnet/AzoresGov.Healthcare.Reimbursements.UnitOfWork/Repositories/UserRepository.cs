244c50b21f45798e3e6680de6e9d9d9d0d6bf761
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 04:17:31 +0100

    User repository

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using AzoresGov.Healthcare.Reimbursements.UnitOfWork.Entities;
+using Datapoint.UnitOfWork.EntityFrameworkCore;
+
+namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
+{
+    public sealed class UserRepository : EntityFrameworkCoreRepository<HealthcareReimbursementsUnitOfWork, User>, IUserRepository
+    {
+        public UserRepository(HealthcareReimbursementsUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
