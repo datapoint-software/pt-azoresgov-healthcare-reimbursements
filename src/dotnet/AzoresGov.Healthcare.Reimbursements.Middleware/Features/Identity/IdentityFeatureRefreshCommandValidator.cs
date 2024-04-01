76c43f8635017a2e62961dfea816264040911fe6
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 06:48:51 +0100

    Identity refresh command validator

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware/Features/Identity/IdentityFeatureRefreshCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.Identity
+{
+    public sealed class IdentityFeatureRefreshCommandValidator : AbstractValidator<IdentityFeatureRefreshCommand>
+    {
+        public IdentityFeatureRefreshCommandValidator()
+        {
+            RuleFor(c => c.UserId)
+                .NotEmpty();
+
+            RuleFor(c => c.UserSessionId)
+                .NotEmpty();
+        }
+    }
+}
