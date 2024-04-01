106130a33374256f2882d2e4dac130266e637587
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 19:02:08 +0100

    User role nature enumeration

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Enumerations/UserRoleNature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AzoresGov.Healthcare.Reimbursements.Enumerations
+{
+    public enum UserRoleNature
+    {
+        Administrative = 'A',
+
+        Administrator = 'S',
+
+        Encoder = 'E',
+
+        Validator = 'V',
+
+        Treasurer = 'T',
+
+        WireTransferAgent = 'W'
+    }
+}
