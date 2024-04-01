e15a98f570542620352d7bcde08daf3a3842ab79
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 19:35:00 +0100

    User roles for sign in and identity refresh

--- src/angular/src/app/api/features/identity/identity-feature-client.abstractions.ts
+++ src/angular/src/app/api/features/identity/identity-feature-client.abstractions.ts
@@ -1,7 +1,10 @@
+import { UserRoleNature } from "../../../app.enums";
+
 export interface IdentityFeatureRefreshResultModel {
   id: string;
   rowVersionId: string;
   name: string;
   emailAddress: string;
   expiration?: string;
+  roles: UserRoleNature[];
 }
--- src/angular/src/app/features/identity/identity-feature.abstractions.ts
+++ src/angular/src/app/features/identity/identity-feature.abstractions.ts
@@ -1,7 +1,10 @@
+import { UserRoleNature } from "../../app.enums";
+
 export type IdentityFeatureClaims = {
   id: string;
   rowVersionId: string;
   name: string;
   emailAddress: string;
   expiration: Date | null;
+  roles: UserRoleNature[];
 }
