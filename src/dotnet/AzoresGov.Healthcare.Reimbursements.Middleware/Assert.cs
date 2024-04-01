a2d25dc178905b80a1086db99e3e25728742b6ca
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-01 07:00:33 +0100

    Entity found assertion methods

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware/Assert.cs
@@ -0,0 +1,35 @@
+﻿using Datapoint;
+using Datapoint.UnitOfWork;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AzoresGov.Healthcare.Reimbursements.Middleware
+{
+    internal static class Assert
+    {
+        internal static void Found<TEntity>([NotNull] TEntity? entity) where TEntity : class, IEntity
+        {
+            if (entity is null)
+                throw new InvalidOperationException("An entity was not found.")
+                    .WithErrorMessage("Os registos associados a esta operação não foram encontrados.");
+        }
+
+        internal static void Found<TEntity>([NotNull] TEntity? entity, [NotNull] Guid? rowVersionId) where TEntity : class, IEntity
+        {
+            if (entity is null)
+            {
+                throw new InvalidOperationException("Entity was not found.")
+                    .WithErrorMessage("A informação necessária não foi encontrada.");
+            }
+
+            if (rowVersionId is null)
+                throw new ConcurrencyException("Row version identifier is missing.");
+
+            if (!entity.PublicId.Equals(rowVersionId))
+            {
+                throw new ConcurrencyException("Row version identifier mismatch.")
+                    .WithErrorMessage("A informação foi alterada entretanto.");
+            }
+        }
+    }
+}
