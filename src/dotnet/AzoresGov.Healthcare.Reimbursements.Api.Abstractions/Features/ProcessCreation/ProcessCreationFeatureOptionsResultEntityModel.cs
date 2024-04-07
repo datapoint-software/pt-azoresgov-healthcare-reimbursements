84b994f06bda2c717b00af7e884165a42ceddd43
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-07 16:38:24 +0100

    Process creation feature options abstractions

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Api.Abstractions/Features/ProcessCreation/ProcessCreationFeatureOptionsResultEntityModel.cs
@@ -0,0 +1,30 @@
+﻿using AzoresGov.Healthcare.Reimbursements.Enumerations;
+using System;
+
+namespace AzoresGov.Healthcare.Reimbursements.Api.Features.ProcessCreation
+{
+    public sealed class ProcessCreationFeatureOptionsResultEntityModel
+    {
+        public ProcessCreationFeatureOptionsResultEntityModel(Guid id, Guid rowVersionId, Guid? parentEntityId, string code, string name, EntityNature nature)
+        {
+            Id = id;
+            RowVersionId = rowVersionId;
+            ParentEntityId = parentEntityId;
+            Code = code;
+            Name = name;
+            Nature = nature;
+        }
+
+        public Guid Id { get; }
+
+        public Guid RowVersionId { get; }
+
+        public Guid? ParentEntityId { get; }
+
+        public string Code { get; }
+
+        public string Name { get; }
+
+        public EntityNature Nature { get; }
+    }
+}
---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Api.Abstractions/Features/ProcessCreation/ProcessCreationFeatureOptionsResultModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AzoresGov.Healthcare.Reimbursements.Api.Features.ProcessCreation
+{
+    public sealed class ProcessCreationFeatureOptionsResultModel
+    {
+        public ProcessCreationFeatureOptionsResultModel(bool entitySelectionEnabled, IReadOnlyCollection<ProcessCreationFeatureOptionsResultEntityModel>? entities, Guid? entityId)
+        {
+            EntitySelectionEnabled = entitySelectionEnabled;
+            Entities = entities;
+            EntityId = entityId;
+        }
+
+        public bool EntitySelectionEnabled { get; }
+
+        public IReadOnlyCollection<ProcessCreationFeatureOptionsResultEntityModel>? Entities { get; }
+
+        public Guid? EntityId { get; }
+    }
+}
