14d170e8766f2ec748d98f0918d74e13b5c6d6fb
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-07 16:42:55 +0100

    Process creation feature options endpoint implementation

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Api/Features/ProcessCreation/ProcessCreationFeatureController.cs
@@ -0,0 +1,45 @@
+﻿using AzoresGov.Healthcare.Reimbursements.Api.Attributes;
+using AzoresGov.Healthcare.Reimbursements.Api.Helpers;
+using AzoresGov.Healthcare.Reimbursements.Middleware.Features.ProcessCreation;
+using Datapoint.Mediator;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzoresGov.Healthcare.Reimbursements.Api.Features.ProcessCreation
+{
+    [Route("/api/features/process-creation")]
+    public sealed class ProcessCreationFeatureController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ProcessCreationFeatureController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Administrative]
+        [HttpGet]
+        public async Task<ProcessCreationFeatureOptionsResultModel> GetOptionsAsync(CancellationToken ct)
+        {
+            var result = await _mediator.HandleQueryAsync<ProcessCreationFeatureOptionsQuery, ProcessCreationFeatureOptionsResult>(
+                new ProcessCreationFeatureOptionsQuery(
+                    User.GetId()),
+                ct);
+
+            return new ProcessCreationFeatureOptionsResultModel(
+                result.EntitySelectionEnabled,
+                result.Entities?
+                    .Select(e => new ProcessCreationFeatureOptionsResultEntityModel(
+                        e.Id,
+                        e.RowVersionId,
+                        e.ParentEntityId,
+                        e.Code,
+                        e.Name,
+                        e.Nature))
+                    .ToArray(),
+                result.EntityId);
+        }
+    }
+}
