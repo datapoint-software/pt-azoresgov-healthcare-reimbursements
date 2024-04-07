2fd387096c03c23bce43dc6fa8d22e2c1d4b4856
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-07 17:58:36 +0100

    Process creation patient selection feature empty implementation

---
+++ src/angular/src/app/features/process-creation-patient-selection/process-creation-patient-selection.feature.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Feature } from "../feature.abstract";
+
+@Injectable()
+export class ProcessCreationPatientSelectionFeature implements Feature {
+
+  // #region State
+
+  // #endregion
+
+  // #region State accessors
+
+  public get complete(): boolean {
+    return false;
+  }
+
+  // #endregion
+
+  // #region Actions
+
+  // #endregion
+
+  constructor() {}
+}
