9f3b7a9619d81b39d3376eef3f60d3ee98cb98b6
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-07 19:28:20 +0100

    Process creation entity selection component form

--- src/angular/src/app/components/process-creation-entity-selection/process-creation-entity-selection.component.ts
+++ src/angular/src/app/components/process-creation-entity-selection/process-creation-entity-selection.component.ts
@@ -1,10 +1,34 @@
 import { Component } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ProcessCreationEntitySelectionFeatureForm } from "../../features/process-creation-entity-selection/process-creation-entity-selection-feature.abstractions";
+import { ProcessCreationEntitySelectionFeature } from "../../features/process-creation-entity-selection/process-creation-entity-selection.feature";
+import { FormGroupComponent } from "../form-group/form-group.component";
 
 @Component({
+  imports: [ FormGroupComponent, ReactiveFormsModule ],
   selector: 'app-process-creation-entity-selection',
   standalone: true,
   templateUrl: './process-creation-entity-selection.component.html'
 })
 export class ProcessCreationEntitySelectionComponent {
 
+  // #region State accessors
+
+  public get form(): ProcessCreationEntitySelectionFeatureForm {
+    return this._processCreationEntitySelection.form;
+  }
+
+  // #endregion
+
+  // #region Actions
+
+  public submit(): void {
+
+  }
+
+  // #endregion
+
+  constructor(
+    private readonly _processCreationEntitySelection: ProcessCreationEntitySelectionFeature
+  ) {}
 }
