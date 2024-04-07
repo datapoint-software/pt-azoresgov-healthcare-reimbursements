9d7eddfd10639d7437dce4bc9d2855addd8afb85
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-07 16:26:35 +0100

    Entity parent entity repository initial implementation

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork/Repositories/EntityParentEntityRepository.cs
@@ -0,0 +1,12 @@
+﻿using AzoresGov.Healthcare.Reimbursements.UnitOfWork.Entities;
+using Datapoint.UnitOfWork.EntityFrameworkCore;
+
+namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
+{
+    public sealed class EntityParentEntityRepository : EntityFrameworkCoreRepository<HealthcareReimbursementsUnitOfWork, EntityParentEntity>, IEntityParentEntityRepository
+    {
+        public EntityParentEntityRepository(HealthcareReimbursementsUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
