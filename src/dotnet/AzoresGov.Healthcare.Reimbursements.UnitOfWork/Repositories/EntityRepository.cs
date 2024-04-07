60e900e6e3b4f72e8183b55283774d32ec25f3a0
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-07 16:03:29 +0100

    Entity repository initial implementation

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.UnitOfWork/Repositories/EntityRepository.cs
@@ -0,0 +1,12 @@
+﻿using AzoresGov.Healthcare.Reimbursements.UnitOfWork.Entities;
+using Datapoint.UnitOfWork.EntityFrameworkCore;
+
+namespace AzoresGov.Healthcare.Reimbursements.UnitOfWork.Repositories
+{
+    public sealed class EntityRepository : EntityFrameworkCoreRepository<HealthcareReimbursementsUnitOfWork, Entity>, IEntityRepository
+    {
+        public EntityRepository(HealthcareReimbursementsUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
