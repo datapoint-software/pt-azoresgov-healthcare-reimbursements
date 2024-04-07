6f013ecdd5c1a95a6f501f0ae6538d49e359d5f2
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-04-07 16:00:55 +0100

    Process creation options query validator

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Middleware/Features/ProcessCreation/ProcessCreationFeatureOptionsQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace AzoresGov.Healthcare.Reimbursements.Middleware.Features.ProcessCreation
+{
+    public sealed class ProcessCreationFeatureOptionsQueryValidator : AbstractValidator<ProcessCreationFeatureOptionsQuery>
+    {
+        public ProcessCreationFeatureOptionsQueryValidator()
+        {
+            RuleFor(q => q.UserId)
+                .NotEmpty();
+        }
+    }
+}
