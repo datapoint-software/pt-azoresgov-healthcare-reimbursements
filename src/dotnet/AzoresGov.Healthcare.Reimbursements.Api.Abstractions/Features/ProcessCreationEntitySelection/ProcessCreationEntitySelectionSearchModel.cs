4360801bd8b8060ddf76312462220fccd04184a4
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-05-16 13:50:27 +0000

    Controller elements for process creation entity selection

---
+++ src/dotnet/AzoresGov.Healthcare.Reimbursements.Api.Abstractions/Features/ProcessCreationEntitySelection/ProcessCreationEntitySelectionSearchModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AzoresGov.Healthcare.Reimbursements.Api.Features.ProcessCreationEntitySelection
+{
+    public sealed class ProcessCreationEntitySelectionSearchModel
+    {
+        public ProcessCreationEntitySelectionSearchModel(string filter, int? skip, int? take)
+        {
+            Filter = filter;
+            Skip = skip;
+            Take = take;
+        }
+
+        public string Filter { get; }
+
+        public int? Skip { get; }
+
+        public int? Take { get; }
+    }
+}
