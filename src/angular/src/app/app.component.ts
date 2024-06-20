b9be7e80adc7093b26346ccee57501a6dfc715c8
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-06-20 13:04:05 +0000

    Navigation loading overlay enqueuement.

--- src/angular/src/app/app.component.ts
+++ src/angular/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { SuiLoadingOverlayComponent } from '@app/components/sui-loading-overlay/sui-loading-overlay.component';
+import { CoreLoadingOverlayFeature } from '@app/features/core-loading-overlay/core-loading-overlay.feature';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,39 @@ import { SuiLoadingOverlayComponent } from '@app/components/sui-loading-overlay/
   templateUrl: 'app.component.html',
   styleUrl: 'app.component.scss'
 })
-export class AppComponent {
-  title = 'azoresgov-healthcare-reimbursements';
+export class AppComponent implements OnInit, OnDestroy {
+
+  private _subscriptions: Subscription[] = undefined!;
+
+  constructor(
+    private readonly _loadingOverlayFeature: CoreLoadingOverlayFeature,
+    private readonly _router: Router
+  ) {}
+
+  ngOnDestroy(): void {
+
+    for (const subscription of this._subscriptions)
+      subscription.unsubscribe;
+
+    this._subscriptions = undefined!;
+  }
+
+  ngOnInit(): void {
+
+    const navigationLoadingOverlayId = `${AppComponent.name}.navigation`;
+
+    this._loadingOverlayFeature.enqueue(navigationLoadingOverlayId);
+
+    this._subscriptions = [
+
+      this._router.events
+        .pipe(filter(e => e instanceof NavigationStart))
+        .subscribe(() => this._loadingOverlayFeature.enqueue(navigationLoadingOverlayId)),
+
+      this._router.events
+        .pipe(filter(e => e instanceof NavigationCancel || e instanceof NavigationEnd || e instanceof NavigationError))
+        .subscribe(() => this._loadingOverlayFeature.dequeue(navigationLoadingOverlayId)),
+    ];
+  }
+
 }
